c9ace49e682a3329c9250f86c6b9b2d13c921a58
19980303 <106376573+19980303@users.noreply.github.com>
2022-05-30 17:24:24 +0900

    initial commit
    練習です。

---
+++ htmlentities.php
@@ -0,0 +1,15 @@
+﻿<html>
+  <head><title>htmlentities</title></head>
+  <body>
+    <form action="htmlentities.php" method="get">
+       <input type="text" name="name">
+       <input type="submit" value="送信">
+     </form>
+  </body>
+</html>
+<?php
+$test = $_GET["name"];
+$test2 = htmlentities($test);
+echo $test . "\n";
+echo $test2;
+?>
